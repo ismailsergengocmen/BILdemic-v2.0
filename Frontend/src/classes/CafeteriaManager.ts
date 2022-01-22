bcc31aa4b5a1b2b2cb6e9e6d4d69eeb6c51644a9
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2022-01-22 20:22:50 +0300

    Cafeteria manager filled

--- Frontend/src/classes/CafeteriaManager.ts
+++ Frontend/src/classes/CafeteriaManager.ts
@@ -1,6 +1,6 @@
-import Meal from './Meal';
+import Meal, { MealType } from './Meal';
 import User from './User';
-import { getDatabase, ref, push, set, get, query, orderByChild, equalTo, onValue } from "firebase/database";
+import { getDatabase, ref, set, get, remove } from "firebase/database";
 import { number } from '@intlify/core-base';
 import { getAuth } from 'firebase/auth';
 
@@ -8,6 +8,7 @@ export default class CafeteriaManager {
   
   //Properties
   private static instance: CafeteriaManager | null = null;
+  private static isBuild: boolean = false;
 
   //Constructor
   private constructor() {}
@@ -20,108 +21,267 @@ export default class CafeteriaManager {
     return this.instance;
   }
 
+  /**
+   * Creates meal order with given parameters
+   * @param place 
+   * @param date 
+   * @param time 
+   * @param type 
+   */
   public async createMealOrder(place: string, date: string, time: string, type: string){
     const ownerUID = getAuth().currentUser?.uid || ' ';
     const db = getDatabase();
     let meal = new Meal(place, time, date, ownerUID, type);
-    let counter78 = 0; // Counter for 78th dorm region
-    let counter51 = 0; // Counter for 51th dorm region
-    let counter90 = 0; // Counter for 90th dorm region
-    let totalCounter: number = 0; // Total counter
+    let val;
 
-    // Hold regional meal counter for regional meal distributiın information
-    if(place == "78" && counter78 != null){
-      counter78 = (await get(ref(db, `PendingMealOrders/Counter78/${counter78}`))).val();
-      (await get(ref(db, `PendingMealOrders/TotalCounter/${totalCounter}`))).val();
-      counter78 = counter78 + 1;
-      totalCounter = totalCounter + 1;
-      await set(ref(db, `PendingMealOrders/Counter78/${counter78}`), counter78);
-      await set(ref(db, `PendingMealOrders/TotalCounter/${totalCounter}`), totalCounter);
+    if(!CafeteriaManager.isBuild){
+      this.buildMealBranch();
+      CafeteriaManager.isBuild = true
     }
 
-    else if(place == "51" && counter51 != null){
-      (await get(ref(db, `PendingMealOrders/Counter51/${counter51}`))).val();
-      (await get(ref(db, `PendingMealOrders/TotalCounter/${totalCounter}`))).val();
-      counter51 = counter51 + 1;
-      totalCounter = totalCounter + 1;
-      await set(ref(db, `PendingMealOrders/Counter51/${counter51}`), counter51);
-      await set(ref(db, `PendingMealOrders/TotalCounter/${totalCounter}`), totalCounter);
+    else{
+      if(place == "78"){
+        val = (await get(ref(db, `PendingMealOrders/Regional/Counter78/Total78/${val}`))).val();
+        val = val + 1;
+        await set(ref(db, `PendingMealOrders/Regional/Counter78/Total78/${val}`), val);
+
+        if(meal.mealType == MealType.NORMAL){
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter78/Normal78/${val}`))).val();
+          val = val + 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter78/Normal78/${val}`), val);
+        }
+
+        else if(meal.mealType == MealType.VEGETARIAN){
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter78/Vegetarian78/${val}`))).val();
+          val = val + 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter78/Vegetarian78/${val}`), val);
+        }
+
+        else{
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter78/Vegan78/${val}`))).val();
+          val = val + 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter78/Vegan78/${val}`), val);
+        }
+      }
+
+      else if(place == "51"){
+        val = (await get(ref(db, `PendingMealOrders/Regional/Counter51/${val}`))).val();
+        val = val + 1;
+        await set(ref(db, `PendingMealOrders/Regional/Counter51/${val}`), val);
+      
+        if(meal.mealType == MealType.NORMAL){
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter51/Normal51/${val}`))).val();
+          val = val + 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter51/Normal51/${val}`), val);
+        }
+
+        else if(meal.mealType == MealType.VEGETARIAN){
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter51/Vegetarian51/${val}`))).val();
+          val = val + 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter51/Vegetarian51/${val}`), val);
+        }
+
+        else{
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter51/Vegan51/${val}`))).val();
+          val = val + 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter51/Vegan51/${val}`), val);
+        }
+      }
+
+      else{
+        val = (await get(ref(db, `PendingMealOrders/Regional/Counter90/Total90/${val}`))).val();
+        val = val + 1;
+        await set(ref(db, `PendingMealOrders/Regional/Counter90/Total90/${val}`), val);
+
+        if(meal.mealType == MealType.NORMAL){
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter90/Normal90/${val}`))).val();
+          val = val + 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter90/Normal90/${val}`), val);
+        }
+
+        else if(meal.mealType == MealType.VEGETARIAN){
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter90/Vegetarian90/${val}`))).val();
+          val = val + 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter90/Vegetarian90/${val}`), val);
+        }
+
+        else{
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter90/Vegan90/${val}`))).val();
+          val = val + 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter90/Vegan90/${val}`), val);
+        }
+      }
+
+      await set(ref(db, `Users/${ownerUID}/Orders/${meal._OID}`), meal);
+      await set(ref(db, `PendingMealOrders/Orders/${meal._OID}`), meal);
+      return true;
+    }
+  }
+
+  /**
+   * Removes given meal order
+   * @param UID, user ID
+   * @param OID, order ID 
+   */
+  public async deleteMealOrder(UID: string, OID: string){
+    const db = getDatabase();
+    let val;
+    
+    let meal = (await get(ref(db, `Users/${UID}/_orders/Sports/${OID}`))).val();
+    let place = meal.place();
+
+      if(place == "78"){
+        val = (await get(ref(db, `PendingMealOrders/Regional/Counter78/Total78/${val}`))).val();
+        val = val - 1;
+        await set(ref(db, `PendingMealOrders/Regional/Counter78/Total78/${val}`), val);
+
+        if(meal.mealType == MealType.NORMAL){
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter78/Normal78/${val}`))).val();
+          val = val - 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter78/Normal78/${val}`), val);
+        }
+
+        else if(meal.mealType == MealType.VEGETARIAN){
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter78/Vegetarian78/${val}`))).val();
+          val = val - 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter78/Vegetarian78/${val}`), val);
+        }
+
+        else{
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter78/Vegan78/${val}`))).val();
+          val = val - 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter78/Vegan78/${val}`), val);
+        }
+      }
+
+      else if(place == "51"){
+        val = (await get(ref(db, `PendingMealOrders/Regional/Counter51/${val}`))).val();
+        val = val - 1;
+        await set(ref(db, `PendingMealOrders/Regional/Counter51/${val}`), val);
+      
+        if(meal.mealType == MealType.NORMAL){
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter51/Normal51/${val}`))).val();
+          val = val - 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter51/Normal51/${val}`), val);
+        }
+
+        else if(meal.mealType == MealType.VEGETARIAN){
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter51/Vegetarian51/${val}`))).val();
+          val = val - 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter51/Vegetarian51/${val}`), val);
+        }
+
+        else{
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter51/Vegan51/${val}`))).val();
+          val = val - 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter51/Vegan51/${val}`), val);
+        }
+      }
+
+      else{
+        val = (await get(ref(db, `PendingMealOrders/Regional/Counter90/Total90/${val}`))).val();
+        val = val - 1;
+        await set(ref(db, `PendingMealOrders/Regional/Counter90/Total90/${val}`), val);
+
+        if(meal.mealType == MealType.NORMAL){
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter90/Normal90/${val}`))).val();
+          val = val - 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter90/Normal90/${val}`), val);
+        }
+
+        else if(meal.mealType == MealType.VEGETARIAN){
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter90/Vegetarian90/${val}`))).val();
+          val = val - 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter90/Vegetarian90/${val}`), val);
+        }
+
+        else{
+          val = (await get(ref(db, `PendingMealOrders/Regional/Counter90/Vegan90/${val}`))).val();
+          val = val - 1;
+          await set(ref(db, `PendingMealOrders/Regional/Counter90/Vegan90/${val}`), val);
+        }
+      }
+
+      await remove(ref(db, `Users/${UID}/_orders/Meals/${OID}`));
+      await remove(ref(db, `PendingMealOrders/Orders/${OID}`));
+      return true;
     }
 
-    else if(place == "90" && counter90 != null){
-      (await get(ref(db, `PendingMealOrders/Counter90/${counter90}`))).val();
-      (await get(ref(db, `PendingMealOrders/TotalCounter/${totalCounter}`))).val();
-      counter90 = counter90 + 1; 
-      totalCounter = totalCounter + 1;
-      await set(ref(db, `PendingMealOrders/Counter90/${counter90}`), counter90);
-      await set(ref(db, `PendingMealOrders/TotalCounter/${totalCounter}`), totalCounter);
-    }
+  /**
+   * Created meal branch and fill counters with 0
+   */
+  public async buildMealBranch(){
+    const db = getDatabase();
 
-    else {
-      counter90 = 0;
-      counter51 = 0;
-      counter78 = 0;
-      totalCounter = 0;
-      await set(ref(db, `PendingMealOrders/Counter78/${counter78}`), counter78);
-      await set(ref(db, `PendingMealOrders/Counter51/${counter51}`), counter51);
-      await set(ref(db, `PendingMealOrders/Counter90/${counter90}`), counter90);
-      await set(ref(db, `PendingMealOrders/TotalCounter/${totalCounter}`), totalCounter);
-    }
+    await set(ref(db, `PendingMealOrders/Regional/Counter90/Total90`), 0);
+    await set(ref(db, `PendingMealOrders/Regional/Counter90/Normal90`), 0);
+    await set(ref(db, `PendingMealOrders/Regional/Counter90/Vegetarian90`), 0);
+    await set(ref(db, `PendingMealOrders/Regional/Counter90/Vegan90`), 0);
 
-    await set(ref(db, `Users/${ownerUID}/Orders/${meal._OID}`), meal);
-    await set(ref(db, `PendingMealOrders/${meal._OID}`), meal);
-    return true;
+    await set(ref(db, `PendingMealOrders/Regional/Counter78/Total78`), 0);
+    await set(ref(db, `PendingMealOrders/Regional/Counter78/Normal78`), 0);
+    await set(ref(db, `PendingMealOrders/Regional/Counter78/Vegetarian78`), 0);
+    await set(ref(db, `PendingMealOrders/Regional/Counter78/Vegan78`), 0);
+
+    await set(ref(db, `PendingMealOrders/Regional/Counter51/Total51`), 0);
+    await set(ref(db, `PendingMealOrders/Regional/Counter51/Normal51`), 0);
+    await set(ref(db, `PendingMealOrders/Regional/Counter51/Vegetarian51`), 0);
+    await set(ref(db, `PendingMealOrders/Regional/Counter51/Vegan51`), 0);
   }
 
   // To change taken status of meal order 
   public async changeTakenStatus(UID:string, OID: string){
     const db = getDatabase();
 
-    let order = (await get(ref(db, `Users/${UID}/Orders/ `))).val();
+    let order = (await get(ref(db, `Users/${UID}/Orders/Meal/`))).val();
     await set(ref(db, `Users/${UID}/Orders/${OID}/${order._taken}`), true);
-    await set(ref(db, `PendingMealOrders/${OID}/${order._taken}`), true);
+    await set(ref(db, `PendingMealOrders/Orders/${OID}/${order._taken}`), true);
     return true;
   }
 
-  // To get regional order count
-  public seeRegionalOrders(place:string): number{
+  /**
+   * To be used in menu based distribution tab
+   * Calculate total order count with specified type
+   * @param type, represent order type which will be calculated 
+   * @param totalType, holds total order count 
+   */
+  public async calculateTotalOrder(type:string, totalType: number){
     const db = getDatabase();
-    let orderCount: number = 0;
-    if(place == "78"){
-      let reference = ref(db, `PendingMealOrders/Counter78/`)
-      onValue(reference, async (snapshot) => {
-        const temp = snapshot.val();
-        orderCount = temp; 
-      })
+    let type90,type78,type51;
+
+    if(type == "normal"){
+      type90 = (await get(ref(db, `PendingMealOrders/Regional/Counter90/Normal90`))).val();
+      type78 = (await get(ref(db, `PendingMealOrders/Regional/Counter78/Normal78`))).val();
+      type51 = (await get(ref(db, `PendingMealOrders/Regional/Counter51/Normal51`))).val();
     }
 
-    if(place == "51"){
-      let reference = ref(db, `PendingMealOrders/Counter51/`)
-      onValue(reference, (snapshot) => {
-        const temp = snapshot.val();
-        orderCount = temp; 
-      })
+    else if(type == "vegeterian"){
+      type90 = (await get(ref(db, `PendingMealOrders/Regional/Counter90/Vegetarian90`))).val();
+      type78 = (await get(ref(db, `PendingMealOrders/Regional/Counter78/Vegetarian78`))).val();
+      type51 = (await get(ref(db, `PendingMealOrders/Regional/Counter51/Vegetarian51`))).val();
     }
 
-    if(place == "90"){
-      let reference = ref(db, `PendingMealOrders/Counter90/`)
-      onValue(reference, (snapshot) => {
-        const temp = snapshot.val();
-        orderCount = temp; 
-      })
+    else if(type == "vegan"){
+      type90 = (await get(ref(db, `PendingMealOrders/Regional/Counter90/Normal90`))).val();
+      type78 = (await get(ref(db, `PendingMealOrders/Regional/Counter78/Normal78`))).val();
+      type51 = (await get(ref(db, `PendingMealOrders/Regional/Counter51/Normal51`))).val();
     }
-    return orderCount;
+
+    else{
+      type90 = (await get(ref(db, `PendingMealOrders/Regional/Counter90/Total90`))).val();
+      type78 = (await get(ref(db, `PendingMealOrders/Regional/Counter78/Total78`))).val();
+      type51 = (await get(ref(db, `PendingMealOrders/Regional/Counter51/Total51`))).val();
+    }
+    totalType = type51 + type78 + type90;
   }
 
-  // To get total order count
-  public seeTotalOrders(): number {
+  /**
+   * To get specific order with takerID
+   * @param takerID 
+   * @param meal 
+   */
+  public async searchOrder(takerID:number,meal:Meal){
     const db = getDatabase();
-    let orderCount: number = 0;
-    let reference = ref(db, `PendingMealOrders/TotalCounter/`)
-    onValue(reference, (snapshot) => {
-      const temp = snapshot.val();
-      orderCount = temp; 
-    })
-    return orderCount;
+    meal = (await get(ref(db, `Users/${takerID}/Orders/Meal/`))).val();
   }
 }
--- Frontend/src/classes/Meal.ts
+++ Frontend/src/classes/Meal.ts
@@ -2,16 +2,33 @@ import { TouchSwipe } from "quasar";
 import Order from "./Order";
 import User from "./User";
 
+
+export enum MealType{
+    NORMAL,
+    VEGETARIAN,
+    VEGAN
+}
+
 export default class Meal extends Order{
     
     // Properties
     private _type: string;
     private _taken: boolean;
+    private _mealType: MealType;
     
     // Constructors
     public constructor(place:string,  date:string, time:string, ownerUID:string, type:string){
         super(place, date, time, ownerUID, "Meal");
-        this._type = type;
+        
+        if(type == "normal"){
+            this._mealType = MealType.NORMAL;
+        }
+        else if(type == "HealthForm"){
+            this._mealType = MealType.VEGETARIAN;
+        }
+        else if(type == "Meal"){
+            this._mealType = MealType.VEGAN;
+        }
     }
 
     // Methods
@@ -19,9 +36,11 @@ export default class Meal extends Order{
     public get OID(): string {
         return this.OID;
     }
+
     public set OID(OID: string) {
         this.OID = OID;
     }
+
     public get type(): string {
         return this._type;
     }
@@ -36,6 +55,14 @@ export default class Meal extends Order{
 
     public set taken(type: boolean) {
         this._taken = type;
+    }
+
+    public get mealType(): MealType {
+        return this._mealType;
+    }
+
+    public set mealType(mealtype: MealType) {
+        this._mealType = mealtype;
     }
 }
 
